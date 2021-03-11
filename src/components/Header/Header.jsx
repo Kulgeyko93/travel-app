d1e1fe597e8af79b9fed68a0e842b157b269bd66
burhonov <38163059+Burhonov@users.noreply.github.com>
2021-03-11 11:10:04 +0500

    feat: project bootstrap

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {
+  selectCountriesStatus,
+  selectCountriesData,
+} from '../../features/countries/countriesSlice';
+import { statuses } from '../../features/constants';
 
 const Header = () => {
   const Main = 'Main';
+  const countriesStatus = useSelector(selectCountriesStatus);
+  const countriesData = useSelector(selectCountriesData);
+
+  let countryLinks = null;
+  if (countriesStatus === statuses.SUCCEEDED) {
+    countryLinks = countriesData.reduce((acc, country) => {
+      const countryLink = (
+        <NavLink key={country.id} to={`/countries/${country.id}`}>
+          {country.name}
+        </NavLink>
+      );
+      return acc === null ? [' ', countryLink] : [...acc, ' ', countryLink];
+    }, null);
+  }
 
   return (
     <div className="Header">
       <NavLink to="/">{Main}</NavLink>
-      <NavLink to="/Country">Country</NavLink>
+      {countryLinks}
     </div>
   );
 };
